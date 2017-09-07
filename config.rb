2c65e1638013646a4a43fdd26b711bd8f75fd615
Taylor Stallworth <tstall17@gmail.com>
2017-09-07 13:31:09 -0500

    Update Config

--- config.rb
+++ config.rb
@@ -13,6 +13,15 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
